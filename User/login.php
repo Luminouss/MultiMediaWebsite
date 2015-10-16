3f7b2b4664a61f351d7546646322b71f52123fff
Luminouss <luminouscsgo@gmail.com>
2015-10-16 17:30:36 +0200

    worked on the login script

--- User/login.php
+++ User/login.php
@@ -5,3 +5,23 @@
  * Date: 16.10.2015
  * Time: 15:50
  */
+/** TODO:Login Script */
+
+$_maxusernamelength = 15;
+$_minpasswordlength = 1;
+
+$_username = $_POST["username"];
+$_password = $_POST["password"];
+
+
+if( $_username && $_password) {
+    if (preg_match("/[^A-Za-z'-]/", $_username)) {
+        die("Name entspricht nicht den Bedingungen");
+    } else if (preg_match("/[^A-Za-z'-]/", $_password)) {
+        die("Passwort entspricht nicht den Bedingungen");
+    }
+
+    echo "Wilkommen " . $_username . " + " . $_password;
+}
+
+?>

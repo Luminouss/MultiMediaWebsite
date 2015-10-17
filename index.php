8684ff41ed88c6181b9c24c5a48868e8e7ef860c
Bugger1 <ariaps42@outlook.de>
2015-10-17 17:56:57 +0200

    animation und Überschrift

--- index.php
+++ index.php
@@ -6,14 +6,40 @@
  * Time: 15:39
  */
 
-$var1 = 53;
-$varmmhmmhmhmshjifhnsdh = 15;
-
-if($var1 > $varmmhmmhmhmshjifhnsdh)
+if (isset($_POST["action_login"]))
 {
-    echo "hi";
-
+    include_once("User.php");
+    $user = new User(new PDOConn(), $_POST["username"], $_POST["password"]);
 }
 
-
 ?>
+
+<!DOCTYPE HTML PUBLIC >
+<html>
+<head lang="de">
+    <meta charset="UTF-8">
+    <title>Index</title>
+    <link href="styles/mainstyle.css" rel="stylesheet" type="text/css">
+    <link href="http://csshake.surge.sh/csshake.min.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<!--TODO:Index erstellen-->
+<!-- TODO:Basic Login Buttons erstellen-->
+
+
+<div class="shake-slow"><h1 class="main-title" title="v0.3">A & D Projekt</h1></div>
+<br><br><br>
+
+<form action="" method="post">
+    Username:<br>
+    <input type="text" name="username">
+    <br>
+    Passwort:<br>
+    <input type="text" name="password"><br>
+
+    <input type="submit" class="loginbuttons" name="action_login" value="Login">
+    <input type="submit" class="loginbuttons" name="action_reg" value="Registrieren">
+
+</form>
+</body>
+</html>

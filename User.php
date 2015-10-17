15d8165a4379b24a4706d268e840e13cc6d787f8
Luminouss <luminouscsgo@gmail.com>
2015-10-17 17:20:26 +0200

    user.php

--- User.php
+++ User.php
@@ -6,8 +6,12 @@
  * Date: 17.10.2015
  * Time: 14:41
  */
+
+include_once("databases/pdo.php");
 class User
 {
+
+    //private $_pdo = new PDOConn();
 
     private $_username;
     private $_password;
